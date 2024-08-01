a0192bc9f1c8727597635ab8a76a0774f67ef688
Eslam Ashraf <SE.eslam.ashraf@gmail.com>
2024-08-01 16:24:13 +0300

    screen util

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,23 +1,29 @@
 import 'package:btl/counter/counter.dart';
 import 'package:btl/l10n/l10n.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        ),
-        useMaterial3: true,
-      ),
-      locale: arabicLocale,
-      supportedLocales: AppLocalizations.supportedLocales,
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      home: const CounterPage(),
+    return ScreenUtilInit(
+      designSize: const Size(393, 852),
+      builder: (context, child) {
+        return MaterialApp(
+          theme: ThemeData(
+            useMaterial3: true,
+            appBarTheme: AppBarTheme(
+              backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            ),
+          ),
+          locale: arabicLocale,
+          supportedLocales: AppLocalizations.supportedLocales,
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          home: const CounterPage(),
+        );
+      },
     );
   }
 }
