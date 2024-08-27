83c627504f5ef59994b42b038cc2df750d2231e6
Eslam Ashraf <SE.eslam.ashraf@gmail.com>
2024-08-27 09:33:32 +0300

    exercise tile

--- lib/features/exercise/presentation/exercises_screen.dart
+++ lib/features/exercise/presentation/exercises_screen.dart
@@ -1,7 +1,11 @@
 import 'package:btl/core/enums/status.dart';
+import 'package:btl/core/extensions/english_x.dart';
+import 'package:btl/core/extensions/text_style_x.dart';
 import 'package:btl/core/theming/app_colors_extension.dart';
+import 'package:btl/core/theming/text_theme_extension.dart';
 import 'package:btl/features/exercise/presentation/bloc/exercise_bloc.dart';
 import 'package:btl/widgets/screen.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:dartx/dartx.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -13,7 +17,7 @@ class ExercisesScreen extends StatefulWidget {
   State<ExercisesScreen> createState() => _ExercisesScreenState();
 }
 
-class _ExercisesScreenState extends State<ExercisesScreen> {
+class _ExercisesScreenState extends State<ExercisesScreen> with AutomaticKeepAliveClientMixin {
   late final ExerciseBloc _bloc;
   late final TextEditingController _searchCntrlr;
 
@@ -26,6 +30,7 @@ class _ExercisesScreenState extends State<ExercisesScreen> {
 
   @override
   Widget build(BuildContext context) {
+    super.build(context);
     return Screen(
       body: BlocBuilder<ExerciseBloc, ExerciseState>(
         builder: (context, state) {
@@ -52,11 +57,30 @@ class _ExercisesScreenState extends State<ExercisesScreen> {
                   Status.loading => const Center(child: CircularProgressIndicator()),
                   _ => state.displayedExercises.isEmpty
                       ? const Center(child: Text('No exercises found'))
-                      : ListView.builder(
+                      : ListView.separated(
                           itemCount: state.displayedExercises.length,
+                          separatorBuilder: (_, __) => const Divider(),
+                          padding: const EdgeInsets.symmetric(vertical: 15),
                           itemBuilder: (context, i) {
+                            final exercise = state.displayedExercises.elementAt(i);
                             return ListTile(
-                              title: Text(state.displayedExercises.elementAt(i).name),
+                              contentPadding: EdgeInsets.zero,
+                              leading: ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: CachedNetworkImage(
+                                  width: 57,
+                                  imageUrl:
+                                      'https://firebasestorage.googleapis.com/v0/b/btl-dev-69d91.appspot.com/o/default_exercises%2Fweighted_crunches.gif?alt=media&token=b17224d9-6542-4292-b600-fd9949a7f913',
+                                ),
+                              ),
+                              title: Text(
+                                exercise.name,
+                                style: context.textThemeX.medium.bold,
+                              ),
+                              subtitle: Text(
+                                '${exercise.level.isNotNullOrBlank ? '${exercise.level!.capitalized} - ' : ''}${exercise.primaryMuscles.first.capitalized}',
+                                style: context.textThemeX.small,
+                              ),
                             );
                           },
                         ),
@@ -68,4 +92,7 @@ class _ExercisesScreenState extends State<ExercisesScreen> {
       ),
     );
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
