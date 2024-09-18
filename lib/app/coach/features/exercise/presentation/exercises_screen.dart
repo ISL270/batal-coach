0ad466cb0249243275e5259f3c901c25ca33e94a
Salah Taha <salahtaha065@gmail.com>
2024-09-18 21:49:04 +0300

    use customscrollview in exercises screen

--- lib/app/coach/features/exercise/presentation/exercises_screen.dart
+++ lib/app/coach/features/exercise/presentation/exercises_screen.dart
@@ -1,10 +1,16 @@
+import 'package:btl/app/app.dart';
 import 'package:btl/app/coach/features/exercise/presentation/bloc/exercise_bloc.dart';
+import 'package:btl/app/coach/features/home/home_screen.dart';
 import 'package:btl/app/core/enums/status.dart';
+import 'package:btl/app/core/extensions/context_x.dart';
 import 'package:btl/app/core/extensions/english_x.dart';
 import 'package:btl/app/core/extensions/scroll_controller_x.dart';
+import 'package:btl/app/core/extensions/string_x.dart';
 import 'package:btl/app/core/extensions/text_style_x.dart';
+import 'package:btl/app/core/l10n/l10n.dart';
 import 'package:btl/app/core/theming/app_colors_extension.dart';
 import 'package:btl/app/core/theming/text_theme_extension.dart';
+import 'package:btl/app/trainee/features/home/home_screen.dart';
 import 'package:btl/app/widgets/screen.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:dartx/dartx.dart';
@@ -33,63 +39,83 @@ class _ExercisesScreenState extends State<ExercisesScreen> with AutomaticKeepAli
           if (state.searchTerm.isBlank) _searchCntrlr.clear();
         },
         builder: (context, state) {
-          return Column(
-            children: [
-              SearchBar(
-                controller: _searchCntrlr,
-                trailing: [
-                  if (state.searchTerm.isNotBlank)
-                    IconButton(
-                      icon: const Icon(Icons.cancel),
-                      color: context.colorsX.onBackgroundTint,
-                      onPressed: () => _bloc.add(ExercisesInitialized()),
+          return CustomScrollView(
+            controller: _scrollCntrlr,
+            slivers: [
+              SliverAppBar(
+                expandedHeight: 200,
+                pinned: true,
+                surfaceTintColor: context.theme.scaffoldBackgroundColor,
+                flexibleSpace: FlexibleSpaceBar(
+                  title: Text(context.l10n.workout(0).capitalizedDefinite),
+                  centerTitle: true,
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: SearchBar(
+                  controller: _searchCntrlr,
+                  trailing: [
+                    if (state.searchTerm.isNotBlank)
+                      IconButton(
+                        icon: const Icon(Icons.cancel),
+                        color: context.colorsX.onBackgroundTint,
+                        onPressed: () => _bloc.add(ExercisesInitialized()),
+                      )
+                  ],
+                  hintText: 'Search exercises',
+                  onChanged: (searchTerm) => _bloc.add(ExerciseSearched(searchTerm)),
+                ),
+              ),
+              const SliverGap(10),
+              if (state.status is Loading) ...[
+                const SliverFillRemaining(
+                  child: Center(child: CircularProgressIndicator()),
+                ),
+              ] else ...[
+                if (state.displayedExercises.isEmpty) ...[
+                  const SliverFillRemaining(
+                    child: Center(
+                      child: Text('No exercises found'),
+                    ),
+                  ),
+                ] else ...[
+                  SliverList.separated(
+                    itemCount: state.displayedExercises.length,
+                    separatorBuilder: (_, __) => const Divider(),
+                    itemBuilder: (context, i) {
+                      final exercise = state.displayedExercises[i];
+                      return ListTile(
+                        contentPadding: EdgeInsets.zero,
+                        leading: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: CachedNetworkImage(
+                            width: 57,
+                            imageUrl:
+                                'https://firebasestorage.googleapis.com/v0/b/btl-dev-69d91.appspot.com/o/default_exercises%2Fweighted_crunches.gif?alt=media&token=b17224d9-6542-4292-b600-fd9949a7f913',
+                          ),
+                        ),
+                        title: Text(
+                          exercise.name,
+                          style: context.textThemeX.medium.bold,
+                        ),
+                        subtitle: Text(
+                          '${exercise.level.isNotNullOrBlank ? '${exercise.level!.capitalized} - ' : ''}${exercise.primaryMuscles.first.capitalized}',
+                          style: context.textThemeX.small,
+                        ),
+                      );
+                    },
+                  ),
+                  if (state.status is PageLoading)
+                    const SliverToBoxAdapter(
+                      child: Padding(
+                        padding: EdgeInsets.all(8),
+                        child: Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                      ),
                     )
                 ],
-                hintText: 'Search exercises',
-                onChanged: (searchTerm) => _bloc.add(ExerciseSearched(searchTerm)),
-              ),
-              const Gap(10),
-              Expanded(
-                child: switch (state.status) {
-                  Loading() => const Center(child: CircularProgressIndicator()),
-                  _ => state.displayedExercises.isEmpty
-                      ? const Center(child: Text('No exercises found'))
-                      : ListView.separated(
-                          physics: const ClampingScrollPhysics(),
-                          controller: _scrollCntrlr,
-                          itemCount: state.hasReachedMax
-                              ? state.displayedExercises.length
-                              : state.displayedExercises.length + 1,
-                          separatorBuilder: (_, __) => const Divider(),
-                          padding: const EdgeInsets.symmetric(vertical: 15),
-                          itemBuilder: (context, i) {
-                            if (i >= state.displayedExercises.length) {
-                              return const Center(child: CircularProgressIndicator());
-                            }
-                            final exercise = state.displayedExercises[i];
-                            return ListTile(
-                              contentPadding: EdgeInsets.zero,
-                              leading: ClipRRect(
-                                borderRadius: BorderRadius.circular(10),
-                                child: CachedNetworkImage(
-                                  width: 57,
-                                  imageUrl:
-                                      'https://firebasestorage.googleapis.com/v0/b/btl-dev-69d91.appspot.com/o/default_exercises%2Fweighted_crunches.gif?alt=media&token=b17224d9-6542-4292-b600-fd9949a7f913',
-                                ),
-                              ),
-                              title: Text(
-                                exercise.name,
-                                style: context.textThemeX.medium.bold,
-                              ),
-                              subtitle: Text(
-                                '${exercise.level.isNotNullOrBlank ? '${exercise.level!.capitalized} - ' : ''}${exercise.primaryMuscles.first.capitalized}',
-                                style: context.textThemeX.small,
-                              ),
-                            );
-                          },
-                        ),
-                },
-              )
+              ]
             ],
           );
         },
