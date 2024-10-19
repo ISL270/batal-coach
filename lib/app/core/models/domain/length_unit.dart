806b9d9fd9e1037d71ad9604e37f3002b1675bcf
Eslam Ashraf <SE.eslam.ashraf@gmail.com>
2024-10-19 22:17:51 +0300

    length unit serialization

--- lib/app/core/models/domain/length_unit.dart
+++ lib/app/core/models/domain/length_unit.dart
@@ -1,32 +1,38 @@
 import 'package:equatable/equatable.dart';
 
+enum LengthUnit { m, km }
+
 sealed class Length extends Equatable {
   final double value;
   const Length(this.value);
+
+  Map<String, dynamic> toJson();
+  static Length fromJson(Map<String, dynamic> json) => switch (json['unit'] as String) {
+        'm' => Meter((json['value'] as num).toDouble()),
+        'km' => Kilometer((json['value'] as num).toDouble()),
+        _ => throw UnimplementedError('Unkonw unit'),
+      };
+
+  @override
+  List<Object?> get props => [value];
 }
 
 class Meter extends Length {
-  static const symbol = 'm';
   const Meter(super.value);
 
   @override
-  String toString() {
-    return '$value $symbol';
-  }
-
-  @override
-  List<Object?> get props => [symbol, value];
+  Map<String, dynamic> toJson() => {
+        'unit': LengthUnit.m.name,
+        'value': value,
+      };
 }
 
 class Kilometer extends Length {
-  static const symbol = 'km';
   const Kilometer(super.value);
 
   @override
-  String toString() {
-    return '$value $symbol';
-  }
-
-  @override
-  List<Object?> get props => [symbol, value];
+  Map<String, dynamic> toJson() => {
+        'type': LengthUnit.km.name,
+        'value': value,
+      };
 }
