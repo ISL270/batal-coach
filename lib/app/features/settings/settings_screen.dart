cb6aec7452599a1049207f6640add780216668cc
Ahmed Sanad <ahmed.hac26@gmail.com>
2024-12-10 18:36:32 +0200

    solve some conflicts

--- lib/app/features/settings/settings_screen.dart
+++ lib/app/features/settings/settings_screen.dart
@@ -13,6 +13,7 @@ import 'package:btl/app/widgets/button.dart';
 import 'package:btl/app/widgets/screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:gap/gap.dart';
 
 class SettingsScreen extends StatelessWidget {
