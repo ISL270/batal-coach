237a831d50207c2e1bbe1094f2faa86e867c736a
Ahmed Sanad <ahmed.hac26@gmail.com>
2024-12-27 07:58:33 +0200

    add missing translations and modify on widgets in settings screen

--- lib/app/features/settings/settings_screen.dart
+++ lib/app/features/settings/settings_screen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: deprecated_member_use
+// ignore_for_file: deprecated_member_use, unused_element
 
 import 'package:btl/app/core/extension_methods/bloc_x.dart';
 import 'package:btl/app/core/extension_methods/english_x.dart';
@@ -7,6 +7,7 @@ import 'package:btl/app/core/extension_methods/text_style_x.dart';
 import 'package:btl/app/core/injection/injection.dart';
 import 'package:btl/app/core/l10n/l10n.dart';
 import 'package:btl/app/core/l10n/language.dart';
+import 'package:btl/app/core/theming/app_colors_extension.dart';
 import 'package:btl/app/core/theming/text_theme_extension.dart';
 import 'package:btl/app/features/authentication/presentation/bloc/auth_bloc.dart';
 import 'package:btl/app/features/settings/settings/settings_bloc.dart';
@@ -36,7 +37,7 @@ class SettingsScreen extends StatelessWidget {
       body: BlocBuilder<SettingsBloc, SettingsState>(
         builder: (context, settings) {
           return Padding(
-            padding: EdgeInsets.symmetric(vertical: 20.w),
+            padding: EdgeInsets.symmetric(vertical: 20.h),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
@@ -72,11 +73,9 @@ class SettingsScreen extends StatelessWidget {
                   widget: SegmentedButton(
                     showSelectedIcon: false,
                     selected: {settings.settings.themeMode},
-                    style:
-                        const ButtonStyle(visualDensity: VisualDensity.compact),
-                    onSelectionChanged: (selection) => context
-                        .read<SettingsBloc>()
-                        .add(SettingsThemeChanged(selection.first)),
+                    style: const ButtonStyle(visualDensity: VisualDensity.compact),
+                    onSelectionChanged: (selection) =>
+                        context.read<SettingsBloc>().add(SettingsThemeChanged(selection.first)),
                     segments: [
                       ButtonSegment(
                         value: ThemeMode.light,
@@ -99,11 +98,9 @@ class SettingsScreen extends StatelessWidget {
                   widget: SegmentedButton(
                     showSelectedIcon: false,
                     selected: {settings.settings.language},
-                    style:
-                        const ButtonStyle(visualDensity: VisualDensity.compact),
+                    style: const ButtonStyle(visualDensity: VisualDensity.compact),
                     onSelectionChanged: (selection) {
-                      context.settingsBloc
-                          .add(SettingsLanguageChanged(selection.first));
+                      context.settingsBloc.add(SettingsLanguageChanged(selection.first));
                     },
                     segments: [
                       ButtonSegment(
@@ -133,24 +130,33 @@ class SettingsScreen extends StatelessWidget {
 }
 
 class _AvatarLabelRow extends StatelessWidget {
-  const _AvatarLabelRow();
+  const _AvatarLabelRow({this.onPressed});
+
+  final void Function()? onPressed;
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      textBaseline: TextBaseline.ideographic,
-      children: [
-        CircleAvatar(radius: 18.w),
-        SizedBox(width: 15.w),
-        Text(
-          context.l10n.profile.capitalized,
-          style: context.textThemeX.medium
-              .copyWith(textBaseline: TextBaseline.ideographic),
+    return InkWell(
+      onTap: onPressed,
+      borderRadius: BorderRadius.circular(16.w),
+      splashColor: context.colorsX.error,
+      child: SizedBox(
+        height: 60.h,
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          textBaseline: TextBaseline.ideographic,
+          children: [
+            CircleAvatar(radius: 18.w),
+            SizedBox(width: 15.w),
+            Text(
+              context.l10n.profile.capitalized,
+              style: context.textThemeX.medium.copyWith(textBaseline: TextBaseline.ideographic),
+            ),
+            const Spacer(),
+            Icon(Icons.arrow_forward_ios_rounded, size: 18.w)
+          ],
         ),
-        const Spacer(),
-        Icon(Icons.arrow_forward_ios_rounded, size: 18.w)
-      ],
+      ),
     );
   }
 }
@@ -160,7 +166,6 @@ class _SettingsSectionWidget extends StatelessWidget {
     required this.label,
     required this.iconData,
     required this.widget,
-    // ignore: unused_element
     this.onPressed,
   });
 
@@ -173,19 +178,23 @@ class _SettingsSectionWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       onTap: onPressed,
-      child: Row(
-        children: [
-          Gap(5.w),
-          Icon(iconData),
-          Gap(20.w),
-          Text(
-            label,
-            style: context.textThemeX.medium
-                .copyWith(textBaseline: TextBaseline.ideographic),
-          ),
-          const Spacer(),
-          widget,
-        ],
+      borderRadius: BorderRadius.circular(16.w),
+      splashColor: context.colorsX.error,
+      child: SizedBox(
+        height: 60.h,
+        child: Row(
+          children: [
+            Gap(5.w),
+            Icon(iconData),
+            Gap(20.w),
+            Text(
+              label,
+              style: context.textThemeX.medium.copyWith(textBaseline: TextBaseline.ideographic),
+            ),
+            const Spacer(),
+            widget,
+          ],
+        ),
       ),
     );
   }
